c00c6f3c9e1bbc54caaec48a2bc20bbd4e7345a0
Georg Dangl <dangl@iabi.eu>
2017-11-14 14:40:53 +0100

    Add npm publish script

---
+++ Xbim.WeXplorer/prepare_npm_bundles.ps1
@@ -0,0 +1,29 @@
+function copyDefinitionFilesToFolder {
+    Param($targetDir)
+    
+	Get-ChildItem $PSScriptRoot -Filter "*.d.ts" `
+		-Recurse | `
+		? { $_.FullName -inotmatch "node_modules" }  | `
+		? { $_.FullName -inotmatch "npm_bundle_output" } | `
+		? { $_.FullName -inotmatch "examples" } | `
+		ForEach-Object { 
+			$targetFile = $targetDir + $_.FullName.SubString($PSScriptRoot.Length); 
+			[void](New-Item -ItemType File -Path $targetFile -Force)
+			Copy-Item $_.FullName -destination $targetFile 
+		} 
+}
+
+# Preparing bundle files for xbim-viewer
+Copy-Item $PSScriptRoot\..\README.md $PSScriptRoot\npm_bundle_output\xbim-viewer\README.md
+Copy-Item $PSScriptRoot\Build\xbim-viewer.js $PSScriptRoot\npm_bundle_output\xbim-viewer\xbim-viewer.js
+Copy-Item $PSScriptRoot\Build\xbim-viewer.min.js $PSScriptRoot\npm_bundle_output\xbim-viewer\xbim-viewer.min.js
+Copy-Item $PSScriptRoot\Build\xbim-geometry-loader.js $PSScriptRoot\npm_bundle_output\xbim-viewer\xbim-geometry-loader.js
+Copy-Item $PSScriptRoot\Build\xbim-geometry-loader.min.js $PSScriptRoot\npm_bundle_output\xbim-viewer\xbim-geometry-loader.min.js
+
+# Preparing bundle files for xbim-browser
+Copy-Item $PSScriptRoot\..\README.md $PSScriptRoot\npm_bundle_output\xbim-browser\README.md
+Copy-Item $PSScriptRoot\Build\xbim-browser.js $PSScriptRoot\npm_bundle_output\xbim-browser\xbim-browser.js
+Copy-Item $PSScriptRoot\Build\xbim-browser.min.js $PSScriptRoot\npm_bundle_output\xbim-browser\xbim-browser.min.js
+
+copyDefinitionFilesToFolder "$PSScriptRoot\npm_bundle_output\xbim-viewer\"
+copyDefinitionFilesToFolder "$PSScriptRoot\npm_bundle_output\xbim-browser\"
