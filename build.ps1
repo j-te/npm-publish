e2e154fac89ec03fb4d408e0d3a03e3ff7fcfe47
Georg Dangl <dangl@iabi.eu>
2017-11-15 11:05:03 +0100

    Change path to nuget

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $VisualStudioVersion = "15.0" # 15 == Visual Studio 2017
 $MsBuildPath = $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion + "MSBuild\15.0\Bin\MSBuild.exe"
 
-& "$PSScriptRoot\Utilities\nuget.exe" restore
+& "$PSScriptRoot\nuget.exe" restore
 
 # Generating build output directory if it doesnt exist
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\Xbim.WeXplorer\Build" | Out-Null
