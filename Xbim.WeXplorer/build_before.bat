156e29a9095d95049496c793d9740fd692904fc2
Georg Dangl <dangl@iabi.eu>
2017-11-15 10:41:03 +0100

    Add build script and update scripts

--- Xbim.WeXplorer/build_before.bat
+++ Xbim.WeXplorer/build_before.bat
@@ -2,4 +2,4 @@ rem Generate JS file containing all shaders as a TypeScript strings. Shaders are
 Utilities\spacker.exe Viewer\shaders shaders.ts -typescript -min -variable:Shaders   && ^
 Utilities\spacker.exe Viewer\plugins\NavigationCube navigation-cube-shaders.ts -typescript -min -variable:CubeShaders 
 
-xcopy /y Viewer\browser\xbim-browser.css Build
+xcopy /y %~dp0Viewer\browser\xbim-browser.css %~dp0Build
---
+++ build.ps1
@@ -0,0 +1,18 @@
+$VisualStudioVersion = "15.0" # 15 == Visual Studio 2017
+$MsBuildPath = $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion + "MSBuild\15.0\Bin\MSBuild.exe"
+
+& "$PSScriptRoot\Utilities\nuget.exe" restore
+
+# Generating build output directory if it doesnt exist
+New-Item -ItemType Directory -Force -Path "$PSScriptRoot\Xbim.WeXplorer\Build" | Out-Null
+
+cd "$PSScriptRoot\Utilities\ShaderPacker"
+& $MsBuildPath
+cd "$PSScriptRoot\Utilities\TypingsBundler"
+& $MsBuildPath
+cd "$PSScriptRoot\Xbim.WeXplorer"
+& npm install
+& $MsBuildPath /p:Configuration=Release
+./prepare_npm_bundles.ps1
+
+cd "$PSScriptRoot"
