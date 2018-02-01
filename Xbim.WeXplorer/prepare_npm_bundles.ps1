eaf890f59cd734d90baa4d6018dc966d7c13f0d0
Georg Dangl <georg@dangl.me>
2018-02-01 21:34:36 +0100

    Update script to include xBim Browser styles

--- Xbim.WeXplorer/prepare_npm_bundles.ps1
+++ Xbim.WeXplorer/prepare_npm_bundles.ps1
@@ -24,6 +24,9 @@ Copy-Item $PSScriptRoot\Build\xbim-geometry-loader.min.js $PSScriptRoot\npm_bund
 Copy-Item $PSScriptRoot\..\README.md $PSScriptRoot\npm_bundle_output\xbim-browser\README.md
 Copy-Item $PSScriptRoot\Build\xbim-browser.js $PSScriptRoot\npm_bundle_output\xbim-browser\xbim-browser.js
 Copy-Item $PSScriptRoot\Build\xbim-browser.min.js $PSScriptRoot\npm_bundle_output\xbim-browser\xbim-browser.min.js
+Copy-Item $PSScriptRoot\Build\xbim-browser.css $PSScriptRoot\npm_bundle_output\xbim-browser\xbim-browser.css
+Remove-Item -Recurse -Force $PSScriptRoot\npm_bundle_output\xbim-browser\Libs\jquery-ui-styles\
+Copy-Item $PSScriptRoot\Libs\jquery-ui-styles\ -Recurse -Destination $PSScriptRoot\npm_bundle_output\xbim-browser\Libs\jquery-ui-styles\ -Container
 
 copyDefinitionFilesToFolder "$PSScriptRoot\npm_bundle_output\xbim-viewer\"
 copyDefinitionFilesToFolder "$PSScriptRoot\npm_bundle_output\xbim-browser\"
